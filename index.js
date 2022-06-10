2d3a8b286cec8b5011374f5aaaa069fe6a28c6dd
Andrea Rosentel <andrearosentel@gmail.com>
2022-06-09 19:25:15 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+let eventName = "surprise";
+
+function writeCards(names, eventName) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+        debugger;
+    }
+    return thankYouCards
+}
+
+writeCards(names, eventName)
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+}
+
+countDown()
